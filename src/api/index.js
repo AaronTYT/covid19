541ffdde58c4c7c69caf3777dcee85fe5d77bc57
AaronTYT <63776864+AaronTYT@users.noreply.github.com>
2020-12-24 20:44:26 +0800

    Successfully added API data for infected, recoveries and deaths

--- src/api/index.js
+++ src/api/index.js
@@ -5,8 +5,8 @@ const url = "https://covid19.mathdro.id/api"
 
 export const fetchData = async () =>{
     try{
+        //data key is required then confirmed then values
         const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url)
-        
         return {confirmed, recovered, deaths, lastUpdate}
         
     }catch(error){
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import {Card, CardContent, Typography, Grid} from "@material-ui/core"
 
+import CountUp from "react-countup"
 import styles from "./Cards.module.css"
 
-function Cards({data: {confirmed, recovered, deaths, lastUpdate}}) {
-    
+function Cards({data:{confirmed, recovered, deaths, lastUpdate}}) {
+    if(!confirmed){
+        return "Loading..."
+    }
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
                  <Grid item component={Card}>
                      <CardContent>
-                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                         <Typography variant="h5">{confirmed.value}</Typography>
-                         <Typography color="textSecondary">Real date</Typography>
+                         <Typography color="textPrimary" gutterBottom>Infected</Typography>
+                         <Typography variant="h5">
+                             <CountUp
+                             start={0}
+                             end={confirmed.value}
+                             duration={2.5}
+                             separator={","}
+                             />
+                         </Typography>
+                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                          <Typography variant="body2">Number of active cases of COVID-19</Typography>
                      </CardContent>
                  </Grid>
@@ -20,8 +30,15 @@ function Cards({data: {confirmed, recovered, deaths, lastUpdate}}) {
                  <Grid item component={Card}>
                      <CardContent>
                          <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                         <Typography variant="h5">Recovered Data</Typography>
-                         <Typography color="textSecondary">Real date</Typography>
+                         <Typography variant="h5">
+                         <CountUp
+                             start={0}
+                             end={recovered.value}
+                             duration={2.5}
+                             separator={","}
+                             />
+                         </Typography>
+                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                          <Typography variant="body2">Number of recoveries cases of COVID-19</Typography>
                      </CardContent>
                  </Grid>
@@ -29,8 +46,15 @@ function Cards({data: {confirmed, recovered, deaths, lastUpdate}}) {
                  <Grid item component={Card}>
                      <CardContent>
                          <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                         <Typography variant="h5">Deaths Data</Typography>
-                         <Typography color="textSecondary">Real date</Typography>
+                         <Typography variant="h5">
+                         <CountUp
+                             start={0}
+                             end={deaths.value}
+                             duration={2.5}
+                             separator={","}
+                        />
+                         </Typography>
+                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                          <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
                      </CardContent>
                  </Grid>
