1ee32aac808f3c4e3b5eb33c5445333946780cb3
Aaron Tan <aaron.tanyutat@yahoo.com.au>
2020-12-24 15:56:08 +0800

    covid19 project started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Cards, Chart, CountryPicker} from './components'
+import styles from './App.module.css'
+import {fetchData} from "./api"
+
+//Functional components (not ES6) does not require lifecycle methods and states
+//Class components (ES6) allows lifecycle methods and states
+
+class App extends React.Component{
+  async componentDidMount(){
+    const data = await fetchData();
+    console.log(data)
+  }
+
+  render(){
+    return(
+      <div className={styles.container}>
+        <Cards/>
+        <CountryPicker/>
+        <Chart/>
+      </div>
+    )
+  }
 }
 
 export default App;
